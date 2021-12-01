e1f43a82c76d21cc3a09b2ea0ae0e8f5b75522e6
D-Aziz <94492261+D-Aziz@users.noreply.github.com>
2021-12-01 14:48:47 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Clock from "./components/profile/profile"
+function App() {
+  return (
+    <div className="App">
+    <Clock/>
+    </div>
+  );
+}
+
+export default App;
